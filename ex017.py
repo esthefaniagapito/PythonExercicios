a84299232788119fd662ddcb133641767be38c20
Esthefani Agapito <esthefaniagapito@gmail.com>
2022-08-24 21:03:18 -0300

    Ex017: fixing error in random lib

--- ex017.py
+++ ex017.py
@@ -1,6 +1,6 @@
 # Leia uma lista de nomes e exiba a sua ordem
 
-from random import shuffle
+import random
 
 nome_01 = input('Nome 01:')
 nome_02 = input('Nome 02:')
@@ -8,7 +8,7 @@ nome_03 = input('Nome 03:')
 nome_04 = input('Nome 04:')
 
 lista_de_alunos = [nome_01, nome_02, nome_03, nome_04]
-ordenado = shuffle(lista_de_alunos)
+random.shuffle(lista_de_alunos)
 
 print('A ordem de apresentação é')
-print(ordenado)
+print(lista_de_alunos)
