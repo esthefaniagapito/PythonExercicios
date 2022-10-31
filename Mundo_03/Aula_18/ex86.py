50b8ce1b161bc1e72aa4e102b8680a479a555b01
Esthefani Agapito <67607326+esthefaniagapito@users.noreply.github.com>
2022-10-31 10:26:41 -0300

    Update ex86.py
    Minha resolução

--- Mundo_03/Aula_18/ex86.py
+++ Mundo_03/Aula_18/ex86.py
@@ -3,7 +3,17 @@ Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3×3
 e preencha com valores lidos pelo teclado.
 No final, mostre a matriz na tela, com a formatação correta.
 """
+#Minha resolução
+matriz = [[], [], []]
+for i in range(0,3):
+    matriz[0].append(int(input()))
+    matriz[1].append(int(input()))
+    matriz[2].append(int(input()))
+for k, v in enumerate(matriz):
+    print(f'{matriz[k]}')
+    print()
 
+# Guanabara
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
         for c in range(0, 3):
