55ee3d6000818b9c3e5b1b59c6db7ce48fbee72d
Esthefani Agapito <67607326+esthefaniagapito@users.noreply.github.com>
2022-10-31 11:41:23 -0300

    Update ex87.py

--- Mundo_03/Aula_18/ex87.py
+++ Mundo_03/Aula_18/ex87.py
@@ -5,6 +5,7 @@ Exercício Python 087: Aprimore o desafio anterior, mostrando no final:
  C) O maior valor da segunda linha.
 """
 
+# Guanabara
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for l in range(0, 3):
         for c in range(0, 3):
@@ -13,3 +14,27 @@ for l in range(0, 3):
     for c in range(0, 3):
             print(f'[{matriz[l][c]:^5}]', end='')
     print()
+
+# Minha resolução
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+par = []
+for l in range(0, 3):
+        for c in range(0, 3):
+                valor = int(input(f'Digite um valor [{l}], [{c}]: '))
+                matriz[l][c] = valor
+                if valor % 2 == 0:
+                    par.append(valor)
+for l in range(0, 3):
+    for c in range(0, 3):
+            print(f'[{matriz[l][c]:^5}]', end='')
+    print()
+print(f'Lista par: {par}')
+
+#Resolução do Guanabara
+for c in range(0, 3):
+    if c == 0:
+        maior = matriz[1][c] # pegando o primeiro elemento na linha
+    elif matriz[1][c] > maior:
+        maior = matriz[1][c]
+print(f'O maior valor da segunda linha é {maior}.')
+
